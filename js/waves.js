91e0ee93d3e10d2a14d8fc3b34d2d8e38f472328
weinnand <weinnandondre.adlaon.hasanion@enrise-corp.co.jp>
2022-03-21 19:09:33 +0800

    Add html, css, and js files

---
+++ js/waves.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  jlottie.loadAnimation({
+    container: document.querySelector("#waves-1"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "./js/data-color-2.json", 
+  });
+});
